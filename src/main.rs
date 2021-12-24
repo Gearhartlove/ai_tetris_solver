e42ba561a84a23b3e9698ed9f0ebf2a027db6ee5
Kristoff <finley@kristoff.codes>
2021-12-24 11:33:04 -0800

    Working on out of bounds functionality

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ mod board;
 
 fn main() {
     let mut board = board::Board::new(10, 20);
-    board.place_block()
-    // block type, position (x,y)
-    //board.place_block(2, 0, &square);
-    // println!("{}", board);
+    // board.place_block(2, 0, Square{});
+    println!("{}", board);
 }
